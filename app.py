a1c16cc4fe9901068af523ce8a8c0001905681e1
Notilis <notilis.concat@gmail.com>
2025-05-17 14:33:36 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+last_data = None
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    global last_data
+    data = request.json.get('data')
+    if not data:
+        return jsonify(error='Brak danych'), 400
+    last_data = data
+    return jsonify(success=True)
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    return jsonify(last_data=last_data)
