56f5c86794a6c751ca894f5ca7bdbecb1e3221d5
Notilis <notilis.concat@gmail.com>
2025-05-18 17:18:52 +0200

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 CORS(app)
 
 last_data = None
+last_data_2 = None
 
 @app.route('/save_data', methods=['POST'])
 def save_data():
@@ -18,6 +19,19 @@ def save_data():
 @app.route('/get_data', methods=['GET'])
 def get_data():
     return jsonify(last_data=last_data)
+
+@app.route('/save_data2', methods=['POST'])
+def save_data2():
+    global last_data_2
+    data = request.json.get('data')
+    if not data:
+        return jsonify(error='Brak danych'), 400
+    last_data_2 = data
+    return jsonify(success=True)
+
+@app.route('/get_data2', methods=['GET'])
+def get_data2():
+    return jsonify(last_data_2=last_data_2)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
