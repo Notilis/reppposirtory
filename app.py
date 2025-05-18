0b6faaec10dfe903d6fa4fe5fe2f5da836ce95e1
Notilis <notilis.concat@gmail.com>
2025-05-18 19:39:38 +0200

    app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ CORS(app)
 
 last_data = None
 last_data_2 = None
+last_data_3 = None
 
 @app.route('/save_data', methods=['POST'])
 def save_data():
@@ -32,6 +33,19 @@ def save_data2():
 @app.route('/get_data2', methods=['GET'])
 def get_data2():
     return jsonify(last_data_2=last_data_2)
+
+@app.route('/save_data3', methods=['POST'])
+def save_data3():
+    global last_data_3
+    data = request.json.get('data')
+    if not data:
+        return jsonify(error='Brak danych'), 400
+    last_data_3 = data
+    return jsonify(success=True)
+
+@app.route('/get_data3', methods=['GET'])
+def get_data3():
+    return jsonify(last_data_3=last_data_3)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
