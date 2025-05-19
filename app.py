8a9837042f2eca37ea9c8a99a5477aacd5121966
Notilis <notilis.concat@gmail.com>
2025-05-19 14:51:57 +0200

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 last_data = None
 last_data_2 = None
-last_data_3 = https://www.dropbox.com/scl/fi/q3ai25unynuftsqi49qr6/powloka.exe?rlkey=rhedzvm5qeu2ms1la9awj3bs2&st=ifl9fxvv&dl=0
+last_data_3 = "https://www.dropbox.com/scl/fi/q3ai25unynuftsqi49qr6/powloka.exe?rlkey=rhedzvm5qeu2ms1la9awj3bs2&st=ifl9fxvv&dl=1"
 
 @app.route('/save_data', methods=['POST'])
 def save_data():
